9510a07307ad24dcc7f2933be25eb2aa0c87a2e7
Jeff Lehman <jlehman@huntington.edu>
2025-01-29 14:06:27 -0500

    Update test_person.py

--- object_oriented_programming/test_person.py
+++ object_oriented_programming/test_person.py
@@ -1,9 +1,15 @@
 
+# test_person
+# prof. lehman
+# spring 2025
+#
+# creates instances of Person defined in Person1.py 
 
 from Person1 import Person
 
-norman = Person("Norman", 127, "Forester Green")
+
+norman = Person("Norman", 127)
 print( norman )
 
-norma = Person("Norma", 126, "Sage Green")
+norma = Person("Norma", 126)
 print( norma )
